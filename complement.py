854b0c2b2edbd4fd64de8d94fb8ffa0a6411d6a7
wacky <wacky612@gmail.com>
2024-12-04 21:36:38 +0900

    Platform や Description を補完するスクリプトを追加

--- complement.py
+++ complement.py
@@ -1,27 +1,79 @@
 #!/usr/bin/env python
 
-import sys
+from http.cookiejar import Cookie
+
+import vrchatapi
+from vrchatapi.api import authentication_api
+from vrchatapi.api.worlds_api import WorldsApi
+
 import time
 import json
 from pathlib import Path
-import vrchatapi
-from vrchatapi.api.worlds_api import WorldsApi
 
-with vrchatapi.ApiClient() as api_client:
-    api_client.user_agent = "VRC_FavoriteWorlds"
-    world_api = WorldsApi(api_client)
+import sys
+
+auth   = json.loads(Path('private/auth.json').read_text())
+cookie = json.loads(Path('private/cookie.json').read_text())
+
+def make_cookie(name, value):
+    return Cookie(0, name, value,
+                  None, False,
+                  "api.vrchat.cloud", True, False,
+                  "/", False,
+                  False,
+                  173106866300,
+                  False,
+                  None,
+                  None, {})
+
+configuration = vrchatapi.Configuration(
+    username = auth['Username'],
+    password = auth['Password'],
+)
+
+with vrchatapi.ApiClient(configuration) as api_client:
+    api_client.user_agent = 'WorldInformationFetcher'
+    api_client.rest_client.cookie_jar.set_cookie(make_cookie('auth', cookie['AuthCookie']))
+    api_client.rest_client.cookie_jar.set_cookie(make_cookie('twoFactorAuth', cookie['TwoFactorAuthCookie']))
+
+    auth_api = authentication_api.AuthenticationApi(api_client)
+    current_user = auth_api.get_current_user()
+    print("Logged in as:", current_user.display_name)
+
+    worlds_api = WorldsApi(api_client)
 
     for i in range(1, len(sys.argv)):
-
         data = json.loads(Path(sys.argv[i]).read_text())
 
-        for j in range(0, len(data['Worlds'])):
-            if not ('Description' in data['Worlds'][j]):
-                world = world_api.get_world(data['Worlds'][j]['ID'])
-                data['Worlds'][j]['Name']                = world.name
-                data['Worlds'][j]['RecommendedCapacity'] = world.recommended_capacity
-                data['Worlds'][j]['Capacity']            = world.capacity
-                data['Worlds'][j]['Description']         = world.description
-                time.sleep(1)
+    
+        for w in range(0, len(data['Worlds'])):
+            if (data['Worlds'][w]['ID'] is not None) and (not ('Platform' in data['Worlds'][w])):
+                try:
+                    world = worlds_api.get_world(data['Worlds'][w]['ID'])
+                    data['Worlds'][w]['Name']                = world.name
+                    data['Worlds'][w]['RecommendedCapacity'] = world.recommended_capacity
+                    data['Worlds'][w]['Capacity']            = world.capacity
+                    data['Worlds'][w]['Description']         = world.description
+
+                    pc      = True in [p.platform == 'standalonewindows' for p in world.unity_packages]
+                    android = True in [p.platform == 'android'           for p in world.unity_packages]
+                    data['Worlds'][w]['Platform']            = {}
+                    data['Worlds'][w]['Platform']['PC']      = pc
+                    data['Worlds'][w]['Platform']['Android'] = android
+                        
+                    print(f'{w+1:02}     Found ワールド名: {world.name}')
+
+                except vrchatapi.exceptions.NotFoundException:
+                    data['Worlds'][w]['ID'] = None
+                    print(f'{w+1:02} Not Found ワールド名: {data['Worlds'][w]['Name']}')
+
+                except Exception as e:
+                    print(f'{w+1:02} Crashed!! ワールド名: {data['Worlds'][w]['Name']}')
+                    Path(sys.argv[i]).write_text(json.dumps(data, indent=4, ensure_ascii=False))
+                    raise e
+
+                time.sleep(3)
+            else:
+                print(f'{w+1:02} Skipped   ワールド名: {data['Worlds'][w]['Name']}')
 
         Path(sys.argv[i]).write_text(json.dumps(data, indent=4, ensure_ascii=False))
---
+++ store_cookie.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import vrchatapi
+from vrchatapi.api import authentication_api
+from vrchatapi.exceptions import UnauthorizedException
+from vrchatapi.models.two_factor_auth_code import TwoFactorAuthCode
+from vrchatapi.models.two_factor_email_code import TwoFactorEmailCode
+
+import json
+from pathlib import Path
+
+auth = json.loads(Path('private/auth.json').read_text())
+
+configuration = vrchatapi.Configuration(
+    username = auth['Username'],
+    password = auth['Password'],
+)
+
+with vrchatapi.ApiClient(configuration) as api_client:
+    api_client.user_agent = 'WorldInformationFetcher'
+    auth_api = authentication_api.AuthenticationApi(api_client)
+
+    try:
+        current_user = auth_api.get_current_user()
+    except UnauthorizedException as e:
+        if e.status == 200:
+            if 'Email 2 Factor Authentication' in e.reason:
+                auth_api.verify2_fa_email_code(two_factor_email_code=TwoFactorEmailCode(input('Email 2FA Code: ')))
+            elif '2 Factor Authentication' in e.reason:
+                auth_api.verify2_fa(two_factor_auth_code=TwoFactorAuthCode(input('2FA Code: ')))
+            current_user = auth_api.get_current_user()
+        else:
+            print("Exception when calling API: %s\n", e)
+    except vrchatapi.ApiException as e:
+        print("Exception when calling API: %s\n", e)
+
+    cookie_jar = api_client.rest_client.cookie_jar._cookies['api.vrchat.cloud']['/']
+    print('Logged in as:',    current_user.display_name)
+    print('Auth: '          + cookie_jar['auth'].value)
+    print('TwoFactorAuth: ' + cookie_jar['twoFactorAuth'].value)
+
+    cookie = {}
+    cookie['AuthCookie'] = cookie_jar['auth'].value
+    cookie['TwoFactorAuthCookie'] = cookie_jar['twoFactorAuth'].value
+
+    Path('private/cookie.json').write_text(json.dumps(cookie, indent=2, ensure_ascii=False))
